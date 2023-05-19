e3ab36cf804040de58c627aa72a7b62f5f141c7b
Tershire <tershire@gmail.com>
2023-05-19 23:05:19 +0200

    normal

--- C++RampUp/L04_03_get_user_string.cpp
+++ C++RampUp/L04_03_get_user_string.cpp
@@ -6,15 +6,24 @@
 #include <iostream>
 #include <cstring>
 
-// CONSTANT ///////////////////////////////////////////////////////////////////
-const int SIZE = 25;
 
 // MAIN ///////////////////////////////////////////////////////////////////////
 int main()
 {
 	using namespace std;
 
+	// CONSTANT ===============================================================
+	const int SIZE = 25;
+	char name[SIZE];
+	char song[SIZE];
 
+	// MAIN ===================================================================
+	cout << "Name: ";
+	// cin.get(name, SIZE); // \n passed onto cin.getline and program finishes
+	cin.get(name, SIZE).get();
+
+	cout << "Song: ";
+	cin.getline(song, SIZE);
 
 	return 0;
 }
