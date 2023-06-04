32ac716aebba3e13e40062849f743ee44d2e2a2e
Tershire <tershire@gmail.com>
2023-06-04 13:12:28 +0200

    Update L04_15_pointer.cpp

--- C++RampUp/L04_15_pointer.cpp
+++ C++RampUp/L04_15_pointer.cpp
@@ -12,11 +12,25 @@ int main()
 {
 	using namespace std;
 
+
+	// FUNDAMENTAL ============================================================
 	int a = 6;
 	int* p_a = &a; // int*: int pointer
 
 	cout << "Value   of a: " <<  a << " == " << *p_a << endl;
 	cout << "Address of a: " << &a << " == " <<  p_a << endl;
+
+
+	// <!> DANGER <!> =========================================================
+	int* p_b;
+	//*p_b = 178; // C6001: using uninitialized memory 'p_b'
+
+	// must assign legitimate address to p_b before using *p_b
+	int b = *p_a + 1;
+	p_b = &b;
+
+	cout << "Value   of b: " << *p_b << endl;
+	cout << "Address of b: " <<  p_b << endl;
 
 	return 0;
 }
